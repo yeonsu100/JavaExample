8361a801a20b4480b062e409ff66f1d563213860
yeonsu100 <yeonsu100@gmail.com>
2020-04-12 23:24:09 +0900

    Array Example 32

---
+++ Algorithm/src/step2_11_array/ArrayEx32.java
@@ -0,0 +1,12 @@
+package step2_11_array;
+
+import java.util.Scanner;
+
+public class ArrayEx32 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		
+		int MAX=5;
+		
+	}
+}
