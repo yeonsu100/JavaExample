28bcb00700269aafcba3e08ba0363b9ff7e4da1e
yeonsu100 <yeonsu100@gmail.com>
2020-04-15 19:13:32 +0900

    Ladder Game (ing)

---
+++ Algorithm/src/exercise_java/LadderGame.java
@@ -0,0 +1,11 @@
+package exercise_java;
+
+public class LadderGame {
+	public static final int STRAIGHT=1;
+	public static final int TURN_RIGHT=2;
+	public static final int TURN_LEFT=3;
+	
+	public static int[][] generateLadder(int width, int height){
+		
+	}
+}
