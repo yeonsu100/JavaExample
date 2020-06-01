89cb41cdafbb94612cd9dabac2b090bd4fb4feb0
yeonsu100 <yeonsu100@gmail.com>
2020-06-01 18:16:54 +0900

    String Example - 01

---
+++ Algorithm/src/step3_14_string/StringEx01.java
@@ -0,0 +1,13 @@
+package step3_14_string;
+
+import java.util.Scanner;
+
+public class StringEx01 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter your name : ");
+		String name=scan.next();
+		
+		System.out.println("Name : "+name);
+	}
+}
