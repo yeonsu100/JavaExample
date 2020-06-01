e5a80cefa4b4f04aa6d25999bb2ea1dccb502754
yeonsu100 <yeonsu100@gmail.com>
2020-06-01 16:47:55 +0900

    Array Example - 42 (Q1)

---
+++ Algorithm/src/step2_11_array/ArrayEx42.java
@@ -0,0 +1,25 @@
+package step2_11_array;
+// 관리비 
+public class ArrayEx42 {
+	public static void main(String[] args) {
+		int[][] apt= {
+				{101, 102, 103},
+				{201, 202, 203},
+				{301, 302, 303}
+		};
+		int[][] hoa= {
+				{1000, 2100, 1300},
+				{4100, 2000, 1000},
+				{3000, 1600, 800}
+		};
+		
+		// Q1) 각 층별 관리비의 합 출력
+		for(int i=0; i<3; i++) {
+			int sum=0;
+			for(int j=0; j<3; j++) {
+				sum+=hoa[i][j];
+			}
+			System.out.println("The sum of "+(i+1)+" floor : "+sum);
+		}
+	}
+}
